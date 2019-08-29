0824bf3b56727359661deefbe63c10a3993f0264
Mbabysbreath <1830817543@qq.com>
2019-08-29 18:03:52 +0800

    增加 快排、冒泡、选择排序（大、小）

--- src/Sort/Sorts1.java
+++ src/Sort/Sorts1.java
@@ -99,6 +99,36 @@ public class Sorts1 {
             swap(array,max,j-1);
         }
     }
+
+    /**
+     * 从一堆元素中选择最大的放在无序区间后边，同时选择最小的放无序区间前边
+     * @param array
+     */
+    public static  void selectSort2(int[] array){
+        int low=0;
+        int high=array.length-1;
+        while(low<high){//无序区间长度【[low,high]
+            //让最大值max和最小值min都从无序区间的第一个位置开始选
+            int max=low;
+            int min=low;
+            //遍历无序区间进行比较
+            for(int i=low;i<=high;i++){
+                if(array[i]>array[max]){
+                    max=i;
+                }
+                if (array[i] < array[min]) {
+                    min=i;
+                }
+            }
+            swap(array,min,low);
+            if(max==low){
+                max=min;
+            }
+            swap(array,max,high);
+            high--;
+            low++;
+        }
+    }
     private static void swap(int array[],int i,int j){
         int temp;
         temp=array[i];
@@ -148,8 +178,128 @@ public class Sorts1 {
         }
         return array;
     }
+
+    /**
+     * 冒泡排序:从数组的第一个元素开始两两比较，将最大的放后边
+     * @param
+     */
+    public static void bubbleSort(int[] array){
+        for(int i=0;i<array.length-1;i++){
+            boolean sorted=true;
+            for(int j=0;j<array.length-i-1;j++){
+                if(array[j]>array[j+1]){
+                    swap(array,j,j+1);
+                    sorted=false;
+                }
+            }
+            if(sorted)
+                break;
+        }
+    }
+
+    /**
+     *快速排序
+     * 找基准值（每次会变）
+     * 比基准值大的放左边，比基准值小的放右边
+     * 在分别对基准值右边的区间进行快排
+     */
+    public static void quickSort(int[] array) {
+        int left=0;
+        int right=array.length-1;
+        quickSortInternal(array,left,right);
+    }
+    public static void quickSortInternal(int[] array,int left,int right){
+        if(left>=right){
+            return;
+        }
+        //选基准值a
+        int privotIndex=partion1(array,left,right);//找基准值的函数
+      //  int[] indice=partion4(array,left,right);
+      //  quickSortInternal(array,left,indice[0]-1);
+       // quickSortInternal(array,indice[1]+1,right);
+        quickSortInternal(array,left,privotIndex-1);
+        quickSortInternal(array,privotIndex+1,right);
+            }
+    private static int partion1(int[] array,int left,int right){//挖坑法获取基准值的位置
+        int pivot=array[right];//基准值
+        int less=left;
+        int great=right;
+        while(less<great){
+            while(less<great&&array[less]<=pivot){
+                less++;
+            }
+            array[great] = array[less];
+            while(less<great&&array[great]>=pivot){
+                great--;
+            }
+            array[less]=array[great];
+        }
+        array[less]=pivot;
+        return less;
+    }
+    private static int partion2(int[] array,int left,int right) {
+        int pivot=array[right];
+        int less=left;
+        int great=right;
+        while(less<great){
+            while(less<great&&array[less]<=pivot){
+                less++;
+            }
+            while(less<great&&array[great]>=pivot){
+                great--;
+            }
+            swap(array,less,great);
+        }
+        swap(array,less,right);
+        return less;
+    }
+
+        private  static int partion3(int[] array,int left,int right){
+        int pivot=array[right];
+            int less=left;//蓝色箭头
+            //[left,less)<privot, [less,i)>=privot;[i,right)未知
+            int i=left;//红色箭头
+            for(i=left;i<right;i++){
+                if(array[i]<pivot){
+                    swap(array,i,less);
+                    less++;
+                }
+            }
+           swap(array,right,less);
+            return less;
+        }
+
+    /**
+     *  //把和基准相同的单独处理，适用于数组中相同元素较多时使用
+     * @param array
+     * @param left
+     * @param right
+     * @return返回一个相同基准元素所在的集合范围
+     */
+        private static int[] partion4(int[] array,int left,int right) {
+            int pivot = array[right];
+            int less = left;
+            int flag=left;
+            int great = right;
+            while (less < great) {
+               if(array[less]==pivot){
+                   less++;
+               }else if(array[less]<pivot){
+                   swap(array,less,flag);
+                   flag++;
+                   less++;
+               }else{
+                   while(less<great&&array[great]>pivot){
+                       great--;
+                   }
+                   swap(array,less,great);
+               }
+            }
+            return new int[]{less,great-1};
+        }
+
     public static void main(String[] args) {
-        int[] array=new int[]{1,8,2,9,2,4,1,5,2,2,4};
+     /*   int[] array=new int[]{1,8,2,9,2,4,1,5,2,2,4};
        // array = buildSortedArray(10);
         System.out.println(Arrays.toString(array));
         long begin = System.nanoTime();//单位是纳秒
@@ -169,6 +319,13 @@ public class Sorts1 {
         long begin3 = System.nanoTime();//单位是纳秒
         heapSort(array);
         long end3=System.nanoTime();
-        System.out.println("希尔排序:"+(end3-begin3));
+        System.out.println("堆排序:"+(end3-begin3));*/
+        int[] array1={1,8,2,9,2,4,1,5,2,2,4};
+        int[] array2=new int[]{7,6,5,4,3,2,1};
+     //   bubbleSort(array1);
+        quickSort(array2);
+        System.out.println(Arrays.toString(array2));
+        selectSort2(array1);
+        System.out.println(Arrays.toString(array1));
     }
 }
